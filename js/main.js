0f5b16c3718f4ffb71c170836eb60aa3fc4a32b7
Максим <vicvega182@gmail.com>
2018-03-23 17:16:11 +0300

    aaa
    aaa

---
+++ js/main.js
@@ -0,0 +1,62 @@
+/* Функция проверки существования элемента */
+$.fn.ex = function () {
+    return $(this).length;
+};
+/* Функция проверки существования элемента */
+
+/* Функция для animate.css */
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function () {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+/* Функция для animate.css */
+
+/* Функция для переключения текста */
+$.fn.extend({
+    toggleText: function (a, b) {
+        var that = this;
+        if (that.text() != a && that.text() != b) {
+            that.text(a);
+        } else if (that.text() == a) {
+            that.text(b);
+        } else if (that.text() == b) {
+            that.text(a);
+        }
+        return this;
+    }
+});
+/* Функция для переключения текста */
+
+$(document).ready(function () {
+    $(document).foundation();
+    new WOW().init();
+
+    /* Scrollbar */
+    if ($("[data-scrollbar]").ex()) {
+        $("[data-scrollbar]").niceScroll();
+    }
+
+    imagesLoaded(document.body, () => document.body.classList.remove('loading'));
+
+    var spriteImages 	= document.querySelectorAll( '.slide-item__image' );
+    var spriteImagesSrc = [];
+
+    for ( var i = 0; i < spriteImages.length; i++ ) {
+        var img = spriteImages[i];
+        spriteImagesSrc.push( img.getAttribute('src' ) );
+    }
+
+    var initCanvasSlideshow = new CanvasSlideshow({
+        sprites: spriteImagesSrc,
+        displacementImage: 'images/clouds.jpg',
+        autoPlay: true,
+        autoPlaySpeed: [10, 3],
+        displaceScale: [200, 70]
+    });
+
+});
+
